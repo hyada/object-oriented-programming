f187704eec125278bd649e8b9e8c5744709c6b83
Hiroyuki Yada <yada@linear-jp.biz>
2022-11-13 23:20:09 +0900

    ソース整理

---
+++ code/sample7-1.php
@@ -0,0 +1,46 @@
+<?php
+
+class Person
+{
+    private $name;
+    private $age;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
+
+class Person2 extends Person
+{
+    private $sex;
+
+    public function __construct($name, $age, $sex)
+    {
+        parent::__construct($name, $age);
+        $this->sex = $sex;
+    }
+
+    public function getSex()
+    {
+        return $this->name.':'.$this->sex;
+    }
+}
+
+$person_a = new Person2('suzuki', 28, 'male');
+$person_b = new Person2('tanaka', 26, 'male');
+
+$person_a_sex = $person_a->getSex();
+
+var_dump($person_a_sex);
